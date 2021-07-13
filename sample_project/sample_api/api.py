c8fb4e2e521444587a8ff2d67436ef8032da1143
Maximilian Wassink <wassink.maximilian@protonmail.com>
2021-07-13 11:45:16 +0200

    added sample api

---
+++ sample_project/sample_api/api.py
@@ -0,0 +1,12 @@
+from ninja import NinjaAPI
+
+from ninja_apikey.security import APIKeyAuth
+
+auth = APIKeyAuth()
+
+api = NinjaAPI(title="Sample API", docs_url="/", auth=auth)
+
+
+@api.get("/hello")
+def hello(request):
+    return f"Hello, {request.user}"
---
+++ sample_project/sample_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SampleApiConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "sample_api"
