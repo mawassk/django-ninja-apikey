1a0312cba3544e8fcd4ba6d849ca981ed5835ec3
Maximilian Wassink <wassink.maximilian@protonmail.com>
2021-07-13 10:29:31 +0200

    first commit

---
+++ ninja_apikey/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class NinjaApikeyConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "ninja_apikey"
+    verbose_name = "API keys"
