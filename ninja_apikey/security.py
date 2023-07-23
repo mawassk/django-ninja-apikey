d1952ecb1a6971ff6696d7a23444e0915e5778b8
AIT ALI EL HOSAYN <44384710+housUnus@users.noreply.github.com>
2023-07-23 23:37:09 +0100

    Update security.py
    Added:
    - APIKeyAuthQuery: to support auth using url
    - Option to specify the API key name during the auth creation
    - Specify certain special permission for the user to have in order to complete the auth

--- ninja_apikey/security.py
+++ ninja_apikey/security.py
@@ -4,7 +4,9 @@ from typing import Any, Optional
 from django.contrib.auth.hashers import check_password, make_password
 from django.http import HttpRequest
 from django.utils.crypto import get_random_string
+from ninja.security import APIKeyQuery
 from ninja.security import APIKeyHeader
+
 
 from .models import APIKey  # type: ignore
 
@@ -18,7 +20,7 @@ def generate_key() -> KeyData:
     return KeyData(prefix, key, hashed_key)
 
 
-def check_apikey(api_key: str) -> Any:
+def check_apikey(api_key: str, perm) -> Any:
     if not api_key:
         return False
 
@@ -31,7 +33,6 @@ def check_apikey(api_key: str) -> Any:
     key = data[1]
 
     persistent_key = APIKey.objects.filter(prefix=prefix).first()
-
     if not persistent_key:
         return False
 
@@ -42,24 +43,54 @@ def check_apikey(api_key: str) -> Any:
         return False
 
     user = persistent_key.user
-
     if not user:
         return False
 
     if not user.is_active:
         return False
-
+    
+    # Additional checking for the view permission for the user
+    if perm and not user.has_perm(perm):
+        return False
     return user
 
-
-class APIKeyAuth(APIKeyHeader):
-    param_name = "X-API-Key"
+# Added the api key as a query option
+class APIKeyAuthQuery(APIKeyQuery):
+    param_name = "api_key"
+    
+    def __init__(self, param_name="api_key", perm="", *args, **kwargs) -> None:
+        self.param_name=param_name
+        self.perm = perm
+        super().__init__()
 
     def authenticate(self, request: HttpRequest, key: Optional[str]) -> Any:
         if not key:
             return False
 
-        user = check_apikey(key)
+        user = check_apikey(key, self.perm)
+
+        if not user:
+            return False
+
+        request.user = user
+        return user
+
+
+class APIKeyAuthHeader(APIKeyHeader):
+    param_name = "api_key"
+    
+    def __init__(self, param_name="api_key", perm="", *args, **kwargs) -> None:
+        self.param_name=param_name
+        #Specify the key used in the url
+        self.perm = perm
+        #If use must have a certain permission to use thtat api
+        super().__init__()
+
+    def authenticate(self, request: HttpRequest, key: Optional[str]) -> Any:
+        if not key:
+            return False
+
+        user = check_apikey(key, self.perm)
 
         if not user:
             return False
